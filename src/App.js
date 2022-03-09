b53e09965e450e0fb96c25a275be4fa53fb18e84
Hannatty <hanatxy@gmail.com>
2022-03-08 23:59:01 -0300

    projeto em react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Equipe extends Component{
+  render(){
+    return(
+      <div>
+        <Sobre nome={this.props.nome} cargo={this.props.cargo} tempo={this.props.tempo}/>
+        <hr/>
+      </div>
+    );
+  }
+}
+class Sobre extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Nome: {this.props.nome}</h2>
+        <h3>Cargo: {this.props.cargo} </h3>
+        <h3>Tempo na empresa: {this.props.tempo} anos</h3>
+      </div>
+    )
+  }
+}
+
+function App () {
+  return(
+    <div>
+      <h1>Quadro de Funcionários - Tech Company</h1>
+      <Equipe nome="Alana Amaral" cargo="Designer de produto" tempo="3"/>
+      <Equipe nome="Caetano Silva" cargo="Desenvolvedor Jr" tempo="2"/>
+      <Equipe nome="Samantha Pires" cargo="Desenvolvedora Senior" tempo="4"/>
+      <Equipe nome="Rafael Cordeiro" cargo="Gerente de Projetos" tempo="5"/>
     </div>
   );
 }
